d2f3ce2e132a1222ae5794ef3609384c61a1c03d
OscarB <oscartic@gmail.com>
2019-01-23 23:13:25 -0300

    add bulma structure (css) and basic components and routes

---
+++ src/views/dashboard/partials/Navbar.vue
@@ -0,0 +1,20 @@
+ <template lang='pug'>
+     #dashboardNavbar
+        .navbar(role="navigation" aria-label="main navigation")
+            .navbar-brand
+                a.navbar-item
+                    img(src="https://bulma.io/images/bulma-logo.png" width="112" height="28")
+            .navbar-menu
+                .navbar-end
+                    .navbar-item
+                        .buttons
+                            a.button.is-danger Cerrar sesión
+ </template>
+
+ <script>
+ export default {
+     name: 'dashboardNavbar'
+ }
+ </script>
+ 
+ 
---
+++ src/views/dashboard/partials/Sidebar.vue
@@ -0,0 +1,19 @@
+ <template lang='pug'>
+     #dashboardSidebar 
+        aside.menu
+            p.menu-label CATEGORIAS
+            ul.menu-list
+                li
+                    router-link(:to="{ name: 'home_path' }")
+                        span.icon
+                            font-awesome-icon(icon='home')
+                        span Inicio
+ </template>
+
+ <script>
+ export default {
+     name: 'dashboardSidebar'
+ }
+ </script>
+ 
+ 
